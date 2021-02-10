ae73ef23ecdaac89c37caf86451f02dfe24470b9
Christopher Bergman <cb.christopherbergman@gmail.com>
2021-02-10 21:31:50 +0100

    Fixed some animations and cleaner code.

--- app/js/script.js
+++ app/js/script.js
@@ -1,41 +1,37 @@
 /*Hamburger menu */
-const btnHamburger = document.querySelector('#btnHamburger');
-const body = document.querySelector('body');
-const header = document.querySelector('.header');
-const overlay = document.querySelector('.overlay');
-const fadeElems = document.querySelectorAll('.has-fade');
+const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
 
-btnHamburger.addEventListener('click', () => {
-  console.log('click hamburger');
+btnHamburger.addEventListener("click", () => {
+  console.log("click hamburger");
 
-  if(header.classList.contains('open')){ // Close Hamburger Menu
-    body.classList.remove('noscroll');
-    header.classList.remove('open');    
+  if (header.classList.contains("open")) {
+    // Close Hamburger Menu
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
     fadeElems.forEach((element) => {
-      element.classList.remove('fade-in');
-      element.classList.add('fade-out');
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
     });
-    
+  } else {
+    // Open Hamburger Menu
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeElems.forEach((element) => {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
   }
-  else { // Open Hamburger Menu
-    body.classList.add('noscroll');
-    header.classList.add('open');
-    fadeElems.forEach((element) => {
-      element.classList.remove('fade-out');
-      element.classList.add('fade-in');
-    });
-
-  }  
 });
 
 //Animation till  about sidan
-new TypeIt('#typeitspan', {
-  strings: '',
-  speed: 70, 
+new TypeIt("#typeitspan", {
+  strings: "",
+  speed: 70,
   afterComplete: function (step, instance) {
     instance.destroy();
-  }
+  },
 }).go();
-
-
-
